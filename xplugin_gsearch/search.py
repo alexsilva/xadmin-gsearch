d6dd778d26cbf9b438258264c9d2031745ee9b37
alexsilva <geniofuturo@gmail.com>
2022-07-18 16:14:25 -0300

    Listview search.

--- xplugin_gsearch/search.py
+++ xplugin_gsearch/search.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 
-class SearchOpts:
-	model = None
+class SearchOptsView:
+	model = opts = None
 
-	def get_queryset(self):
-		raise NotImplementedError('get_queryset')
+	def get_label(self):
+		return self.opts.verbose_name
+
+	def get_total(self):
+		return self.get_list_queryset().count()
 
 
 class Search:
@@ -13,30 +16,29 @@ class Search:
 	def __init__(self):
 		self.registry = {}
 		self.cache = {}
+		self._iterator = None
 
 	def register(self, model, option_class):
-		self.registry.setdefault(model, []).append(option_class)
+		try:
+			model_options = self.registry[model]
+		except KeyError:
+			self.registry[model] = model_options = []
+		model_options.append(option_class)
 
-	def get_option(self, model):
+	def __iter__(self):
+		return iter(self.registry)
+
+	def get_option(self, model, option_base):
 		if model in self.cache:
 			return self.cache[model]
 		opts = model._meta
-		options_classes = self.registry[model]
-		options = type("".join([opts.__name__ for opts in options_classes] + [opts.app_lable, opts.model_name]),
-		               options_classes, {
+		bases = [option_base, SearchOptsView] + self.registry[model]
+		options = type("".join([opts.__name__ for opts in bases] + [opts.app_label, opts.model_name]),
+		               tuple(bases), {
 			               'model': model
 		               })
 		self.cache[model] = options
 		return options
-
-	def get_result_count(self, *models):
-		total = 0
-		for model in models:
-			if model not in self.registry:
-				continue
-			option = self.get_option(model)
-			total += option.get_queryset()
-		return total
 
 
 search = Search()
--- xplugin_gsearch/views/search.py
+++ xplugin_gsearch/views/search.py
@@ -1,15 +1,34 @@
 # coding=utf-8
 from django.template.response import TemplateResponse
-from xadmin.views import CommAdminView
+from xadmin.views import CommAdminView, ListAdminView
 from xplugin_gsearch.search import search
 
 
 class GlobalSearchView(CommAdminView):
 	template_name = "gsearch/search.html"
 
+	def get_search_view(self, model_option):
+		return self.get_view(ListAdminView, model_option)
+
 	def get(self, request, **kwargs):
 		context = self.get_context()
-		context['gsearch'] = search
+		views = []
+		count = 0
+		for model in search:
+			opts = self.admin_site.get_registry(model)
+			model_option = search.get_option(model, opts)
+			search_view = self.get_search_view(model_option)
+			query_string = search_view.get_query_string()
+			views.append({
+				'view': search_view,
+				'url': search_view.model_admin_url("changelist") + query_string
+			})
+			count += search_view.get_total()
+		context['gsearch'] = {
+			'url': self.get_admin_url("gsearch"),
+			'count': count,
+			'views': views
+		}
 		response = TemplateResponse(
 			request,
 			self.template_name,
