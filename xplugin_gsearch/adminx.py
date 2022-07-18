67d1bcac3e5bf154bb3dded1237043fc42e22238
alexsilva <geniofuturo@gmail.com>
2022-07-18 17:27:41 -0300

    Global search plugin.

--- xplugin_gsearch/adminx.py
+++ xplugin_gsearch/adminx.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 from xadmin.sites import site
+from xadmin.views.base import CommAdminView
+from xplugin_gsearch.plugin import GlobalSearchPlugin
 from xplugin_gsearch.views.search import GlobalSearchView
 
 site.register_view("gsearch/", GlobalSearchView, "gsearch")
+site.register_plugin(GlobalSearchPlugin, CommAdminView)
---
+++ xplugin_gsearch/plugin.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from django.template.loader import render_to_string
+from xadmin.filters import SEARCH_VAR
+from xadmin.plugins.utils import get_context_dict
+from xadmin.views.base import BaseAdminPlugin
+from xplugin_gsearch.views.search import GlobalSearchView
+
+
+class GlobalSearchPlugin(BaseAdminPlugin):
+	global_search_template = "gsearch/blocks/search.nav.top.html"
+
+	def init_request(self, *args, **kwargs):
+		return not isinstance(self.admin_view, GlobalSearchView)
+
+	def setup(self, *args, **kwargs):
+		self.search_url = self.admin_view.get_admin_url("gsearch")
+
+	def block_top_navmenu(self, context, nodes):
+		context = get_context_dict(context or {})
+		context['gsearch'] = {
+			'url': self.search_url,
+			'param': SEARCH_VAR
+		}
+		nodes.append(
+			render_to_string(self.global_search_template,
+			                 context=context)
+		)
+
+	block_top_navmenu.priority = 1000
