ef97b56354415db369bbd87fc694cb1b6adbde29
alexsilva <geniofuturo@gmail.com>
2022-07-18 17:42:55 -0300

    Deactivation of the plugin in the listview.

--- xplugin_gsearch/plugin.py
+++ xplugin_gsearch/plugin.py
@@ -2,6 +2,7 @@
 from django.template.loader import render_to_string
 from xadmin.filters import SEARCH_VAR
 from xadmin.plugins.utils import get_context_dict
+from xadmin.views import ListAdminView
 from xadmin.views.base import BaseAdminPlugin
 from xplugin_gsearch.views.search import GlobalSearchView
 
@@ -10,7 +11,7 @@ class GlobalSearchPlugin(BaseAdminPlugin):
 	global_search_template = "gsearch/blocks/search.nav.top.html"
 
 	def init_request(self, *args, **kwargs):
-		return not isinstance(self.admin_view, GlobalSearchView)
+		return not isinstance(self.admin_view, (GlobalSearchView, ListAdminView))
 
 	def setup(self, *args, **kwargs):
 		self.search_url = self.admin_view.get_admin_url("gsearch")
@@ -19,7 +20,8 @@ class GlobalSearchPlugin(BaseAdminPlugin):
 		context = get_context_dict(context or {})
 		context['gsearch'] = {
 			'url': self.search_url,
-			'param': SEARCH_VAR
+			'param': SEARCH_VAR,
+			'val': self.request.GET.get(SEARCH_VAR, '')
 		}
 		nodes.append(
 			render_to_string(self.global_search_template,
