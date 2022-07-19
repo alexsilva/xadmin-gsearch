7e91b4c14304421788aed87eeb303e9f5cf4637c
alexsilva <geniofuturo@gmail.com>
2022-07-19 10:31:56 -0300

    Add setup.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+	name='xadmin-gsearch-plugin',
+	version='1.0.0',
+	include_package_data=True,
+	packages=['xplugin_gsearch', 'xplugin_gsearch.views'],
+	url='https://github.com/alexsilva/xadmin-gsearch-plugin',
+	license='MIT',
+	author='alex',
+	author_email='',
+	description='xadmin global search'
+)
