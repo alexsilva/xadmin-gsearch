b7d232f5c8824aa0e17886149804dfd822431bdb
alexsilva <geniofuturo@gmail.com>
2022-07-22 16:25:53 -0300

    Search input always visible.

--- xplugin_gsearch/plugin.py
+++ xplugin_gsearch/plugin.py
@@ -2,18 +2,15 @@
 from django.template.loader import render_to_string
 from xadmin.filters import SEARCH_VAR
 from xadmin.plugins.utils import get_context_dict
-from xadmin.views import ListAdminView
 from xadmin.views.base import BaseAdminPlugin
 from xplugin_gsearch.search import search
-from xplugin_gsearch.views.search import GlobalSearchView
 
 
 class GlobalSearchPlugin(BaseAdminPlugin):
 	global_search_template = "gsearch/blocks/search.nav.top.html"
 
 	def init_request(self, *args, **kwargs):
-		return (not isinstance(self.admin_view, (GlobalSearchView, ListAdminView)) and
-		        len(search))
+		return bool(len(search))
 
 	def setup(self, *args, **kwargs):
 		self.search_url = self.admin_view.get_admin_url("gsearch")
