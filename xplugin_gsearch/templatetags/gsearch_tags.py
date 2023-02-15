b5cc6a5de40090108fb48403cd77367aeeb804a8
alexsilva <geniofuturo@gmail.com>
2023-02-15 15:48:45 -0300

    #1758: adding more filters.

--- xplugin_gsearch/templatetags/gsearch_tags.py
+++ xplugin_gsearch/templatetags/gsearch_tags.py
@@ -8,3 +8,13 @@ register = template.Library()
 def filters_num(search_views):
 	"""filters and counts the total number of views marked as a filter."""
 	return len([view for view in search_views if view['checked']])
+
+
+@register.simple_tag
+def filters_all_checked(search_views):
+	all_checked = True
+	for view in search_views:
+		if not view['checked']:
+			all_checked = False
+			break
+	return all_checked
