0db98443a87116084f62aa37bd3b3872083e0e6f
alexsilva <geniofuturo@gmail.com>
2023-02-15 14:40:16 -0300

    code reformat.

--- xplugin_gsearch/views/search.py
+++ xplugin_gsearch/views/search.py
@@ -31,6 +31,7 @@ class SearchForm(django_forms.Form):
 
 class CommSearchView(CommAdminView):
 	"""Common search"""
+
 	def get_search_view(self, model_option, **opts):
 		view = self.get_view(ListAdminView, model_option, opts=opts)
 		if not getattr(view, "search_fields", None):
@@ -61,19 +62,19 @@ class GlobalSearchView(CommSearchView):
 	def block_nav_menu(self, context, nodes):
 		context = get_context_dict(context or {})
 		nodes.append(render_to_string('gsearch/blocks/search.nav_menu.filters.html',
-												 context=context))
+		                              context=context))
 
 	def block_nav_btns(self, context, nodes):
 		context = get_context_dict(context or {})
 		nodes.append(render_to_string("gsearch/blocks/search.nav_btns.filters.html",
-									  context=context,
-									  request=self.request))
+		                              context=context,
+		                              request=self.request))
 
 	def block_nav_toggles(self, context, nodes):
 		context = get_context_dict(context or {})
 		nodes.append(render_to_string("gsearch/blocks/search.navmob_toggles.filters.html",
-			                              context=context,
-			                              request=self.request))
+		                              context=context,
+		                              request=self.request))
 
 	def get_breadcrumb(self):
 		bc = super().get_breadcrumb()
