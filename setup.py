4570eeadfa90fb6ee0cd56e1530a966baf20d854
alexsilva <geniofuturo@gmail.com>
2023-02-15 17:39:55 -0300

    #1758: add 'templatetags' to install.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	name='xadmin-gsearch-plugin',
 	version='1.0.3',
 	include_package_data=True,
-	packages=['xplugin_gsearch', 'xplugin_gsearch.views'],
+	packages=['xplugin_gsearch', 'xplugin_gsearch.templatetags', 'xplugin_gsearch.views'],
 	url='https://github.com/alexsilva/xadmin-gsearch-plugin',
 	license='MIT',
 	author='alex',
